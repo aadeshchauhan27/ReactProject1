f549f8a7bf5a2442485d37dcb38d4050b229e065
Chauhan Aadeshsingh <Chauhaadd@ap.dtcs.local>
2021-03-07 01:57:41 +0530

    project setup

---
+++ app/Main.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
+
+function Main() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+
+        <Route path="/about-us">
+          <About />
+        </Route>
+
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"));
---
+++ app/components/Header.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import HeaderLoggedOut from "./HeaderLoggedOut";
+import HeaderLoggedIn from "./HeaderLoggedIn.js";
+
+function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState();
+
+  return (
+    <header className="header-bar bg-primary mb-3">
+      <div className="container d-flex flex-column flex-md-row align-items-center p-3">
+        <h4 className="my-0 mr-md-auto font-weight-normal">
+          <a href="/" className="text-white">
+            {" "}
+            ComplexApp{" "}
+          </a>
+        </h4>
+        {isLoggedIn ? (
+          <HeaderLoggedIn setIsLoggedIn={setIsLoggedIn} />
+        ) : (
+          <HeaderLoggedOut setIsLoggedIn={setIsLoggedIn} />
+        )}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
